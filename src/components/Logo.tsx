a778b810c98934b686b0d3b28f2b2ad88ff7ef96
Old <nko.goh@gmail.com>
2017-11-05 16:26:10 +0100

    fix logo link, add favicon

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {Link} from "react-router-dom";
+import {LinkWrapper} from "./LinkWrapper";
 
 interface LogoProps{
   fontSize?: number;
@@ -10,14 +11,16 @@ export class Logo extends React.Component<LogoProps, any>{
     let style: any = {};
     if(this.props.fontSize) style.fontSize = this.props.fontSize + 'px';
     return (
-      <Link to="/" className="main-logo" style={style}>
-        <span>T</span>
-        <span>i</span>
-        <span>b</span>
-        <span>o</span>
-        <span>l</span>
-        <span>d</span>
-      </Link>
+      <LinkWrapper link={'/'}>
+        <div className="main-logo" style={style}>
+          <span>T</span>
+          <span>i</span>
+          <span>b</span>
+          <span>o</span>
+          <span>l</span>
+          <span>d</span>
+        </div>
+      </LinkWrapper>
     )
   }
 }
